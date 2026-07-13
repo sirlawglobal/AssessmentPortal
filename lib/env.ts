e5be90ce43f6ca28963b1c72d9defe72215f04dc
Lawrence <80970589+sirlawglobal@users.noreply.github.com>
2026-07-13 13:36:40 +0100

    Update env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   MONGODB_URI: z.string().min(1).optional(),
   NEXTAUTH_SECRET: z.string().min(1).optional(),
   NEXTAUTH_URL: z.string().url().optional(),
+  CLOUDINARY_URL: z.string().optional(),
   NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
 });
 
